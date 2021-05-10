9a9c1eab404c59dec843bd0a26a9f97ec4134435
rodericusifo <rodericus1999@gmail.com>
2021-05-11 00:30:14 +0700

    update for deploying

---
+++ server.js
@@ -0,0 +1,20 @@
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+const compression = require("compression");
+const express = require("express");
+const app = express();
+
+app.use(compression());
+app.use(requireHTTPS);
+app.use(express.static("./dist/green-shop-frontend"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/green-shop-frontend/" });
+});
+
+app.listen(process.env.PORT || 8080);
